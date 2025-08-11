fdb7ad25d2299d7c995fc28e6715b69b3557d6f6
Wambui Kibathi <zeldabobo02@gmail.com>
2025-08-11 19:59:44 +0300

    Complete Anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = ''.join(sorted(self.word))
+
+    def is_anagram(self, candidate):
+        return candidate.lower() != self.word and \
+               ''.join(sorted(candidate.lower())) == self.sorted_word
+
+    def match(self, candidates):
+        matches = []
+        for candidate in candidates:
+            if isinstance(candidate, str) and self.is_anagram(candidate):
+                matches.append(candidate)
+        return matches
